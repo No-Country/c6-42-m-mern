2377d742b84922e716e8dd5f455774f44532c0fe
requin883 <requin883@gmail.com>
2022-08-14 19:41:15 -0700

    feat: create signupValidation file

---
+++ Back/src/Utils/signupValidation.js
@@ -0,0 +1,32 @@
+import userModel from "../models/UserModel";
+
+export const signUp = async (req, res) => {
+    try {
+        const { username, Firstname, lastName, phoneNumber, dni, address, email, password, dateOfBirth, tutor = null } = req.body;
+
+        const userExists = await userModel.findOne({ email });
+
+        if (userExists)
+            return res.status(409).json({ message: "User aready exists" });
+
+        const newUser = new userModel({
+            username,
+            Firstname,
+            lastName,
+            phoneNumber,
+            dni,
+            dateOfBirth,
+            address,
+            tutor,
+            email,
+            password: await userModel.encryptPassword(password),
+
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ message: "Somethin went wrong, try again" });
+    }
+};
+
+export default signUp;
