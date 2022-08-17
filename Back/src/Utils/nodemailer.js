e9025da4b6290457f7b4809eb1ae2cd8296cef09
Adrian <esteban.adrian.gonz@gmail.com>
2022-08-17 00:21:41 -0300

    Removidos datos innecesarios

--- Back/src/Utils/nodemailer.js
+++ Back/src/Utils/nodemailer.js
@@ -1,19 +1,18 @@
 const { createTransport } = require("nodemailer");
 
-const email = 'sistemadereserva1@gmail.com';
-
 const transporter = createTransport({
-  host: 'smtp.ethereal.email',
+  service: 'gmail',
   port: 587,
   auth: {
-      user: 'rhoda.fahey76@ethereal.email',
-      pass: 'M7v2BHh28w8pYMgpq4'
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PW
   }
 });
 
 const mailOptions = {
   from: 'Sitio de Reservas de Canchas Deportivas',
-  to: email,
+  // Cambiar to: hacia quien vaya dirigido (el email del usuario)
+  to: process.env.NODEMAILER_USER,
   subject: 'Confirmacion de cuenta',
   html: '<h3>Por favor acive su cuenta clickeando el siguiente enlace <a href="http://localhost:8080/activar-cuenta">ACTIVAR CUENTA</a></h3>'
 };
