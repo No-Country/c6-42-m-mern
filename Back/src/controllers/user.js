a4d1d7bb9f42fc93cf176c63b973341c573fb004
requin883 <requin883@gmail.com>
2022-08-20 08:06:57 -0700

    feat: Create token, create confirmation link and send email on account creation

--- Back/src/controllers/user.js
+++ Back/src/controllers/user.js
@@ -1,6 +1,8 @@
+require("dotenv").config({ path: "../../.env" })
 const mongoose = require("mongoose");
 const userSchema = require("../models/userModel");
-
+const jwt = require("jsonwebtoken");
+const { transporter, mailOptions } = require("../Utils/nodemailer");
 class User {
   constructor(collection) {
     this.collection = collection;
@@ -10,7 +12,7 @@ class User {
     const model = mongoose.model(this.collection);
     const user = await model.find({ username: usr });
     if (user.length === 0) return false;
-    return user; 
+    return user;
   };
 
   async create({ firstName, lastName, dateOfBirth, dni, city, street, email, role, phoneNumber, password, username }) {
@@ -29,17 +31,30 @@ class User {
       phoneNumber,
       password: await userSchema.encryptPassword(password)
     });
+    const userToken = jwt.sign({
+      id: newUser._id,
+      username: newUser.username,
+      email: newUser.email
+    }, process.env.JWT_SECRET,
+      {
+        expiresIn: "1h"
+      })
+    newUser.verToken = userToken;
+    const confirmationLink = `https://localhost:5000/activar-cuenta/${userToken}`;
+    await transporter.sendMail(mailOptions(confirmationLink, newUser.email));
     const res = await newUser.save();
+
+
     return res;
   };
 
-  find() {};
+  find() { };
 
-  findAll() {};
+  findAll() { };
 
-  delete() {};
+  delete() { };
 
-  update() {};
+  update() { };
 };
 
 module.exports = new User('users');
