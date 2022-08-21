58df627adcc147fb65a125404cb8d6c4a23b48b0
requin883 <requin883@gmail.com>
2022-08-21 11:16:09 -0700

    feat: add resetToken to userSchema

--- Back/src/models/userModel.js
+++ Back/src/models/userModel.js
@@ -50,6 +50,7 @@ const userSchema = new mongoose.Schema(
             },
         },
         verToken:String,
+        resetToken:String,
         confirmedAccount:{
             type:Boolean,
             default:false
