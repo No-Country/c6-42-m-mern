6d158ff9e18f31908055029cc8c349a55ecb6023
requin883 <requin883@gmail.com>
2022-08-21 10:52:42 -0700

    feat: change validation form for mailOptions to use it for 2 different validations

--- Back/src/Utils/nodemailer.js
+++ Back/src/Utils/nodemailer.js
@@ -4,29 +4,41 @@ const transporter = createTransport({
   service: 'gmail',
   port: 587,
   auth: {
-      user: process.env.NODEMAILER_USER,
-      pass: process.env.NODEMAILER_PW
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PW
   }
 });
 
 const contactMailOptions = (values) => {
-  let {name,club,email,subject,message} = values;
+  let { name, club, email, subject, message } = values;
   return {
     from: `${name} -- ${email}`,
     to: process.env.NODEMAILER_USER,
-    subject:`Este mensaje es para el club ${club} : ${subject}`,
-    text:message
-    }
+    subject: `Este mensaje es para el club ${club} : ${subject}`,
+    text: message
+  }
 }
 
-const mailOptions = (confirmationLink,userEmail) =>{
-  return {
-  from: "Sistema de reservas deportivas",
-  // Cambiar to: hacia quien vaya dirigido (el email del usuario)
-  to: userEmail,
-  cc:process.env.NODEMAILER_USER,
-  subject: 'Confirmacion de cuenta',
-  html: `<h3>Por favor active su cuenta clickeando el siguiente enlace <a href="${confirmationLink}">ACTIVAR CUENTA</a></h3>`
-}}
+const mailOptions = (confirmationLink, userEmail, option) => {
+  from = "Sistema de reservas deportivas";
+  if (option == "activation") {
+    return {
+      from,
+      to: userEmail,
+      cc: process.env.NODEMAILER_USER,
+      subject: 'Confirmacion de cuenta',
+      html: `<h3>Por favor active su cuenta clickeando el siguiente enlace <a href="${confirmationLink}">ACTIVAR CUENTA</a></h3>`
+    }
+  }
+  if (option === "renewpass") {
+    return {
+      from,
+      to: userEmail,
+      cc: process.env.NODEMAILER_USER,
+      subject: 'Reinicio de contraseña',
+      html: `<h3>Por favor actualice su contraseña clickeando el siguiente enlace <a href="${confirmationLink}">Actualizar contraseña</a></h3>`
+    }
+  }
+}
 
-module.exports = { transporter, mailOptions, contactMailOptions };
+module.exports = { transporter, mailOptions, contactMailOptions, passwordMailOptions };
