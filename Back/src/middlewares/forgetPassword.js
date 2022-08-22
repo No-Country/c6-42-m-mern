654d6659e78b35ad529742fc1afc18c3557220eb
Adrian <esteban.adrian.gonz@gmail.com>
2022-08-22 15:29:28 -0300

    Integrado middlewares y manejo de pw

---
+++ Back/src/middlewares/forgetPassword.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const userSchema = require("../models/userModel");
+const { transporter, passwordMailOptions } = require("../Utils/nodemailer");
+
+const forgetPassword = async (req, res) => {
+  try {
+    let { email } = req.params;
+
+    if (!email) return res.status(404).json({ message: "Se necesita una dirección de correo electrónico" })
+
+    const user = await userSchema.findOne({ email });
+
+    if (!user) return res.status(403)
+
+    const token = jwt.sign({
+      id: user._id,
+      username: user.username,
+      password: user.password
+    },
+      process.env.JWT_SECRET,
+      { expiresIn: "15m" })
+
+
+    user.resetToken = token;
+
+    const confirmationLink = `http://localhost:5000/new-password/${token}`
+
+    await user.save();
+
+    await transporter.sendMail(passwordMailOptions(confirmationLink, user.email, "renewpass"));
+
+    return res.status(200).json({ message: "Email sent" })
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = forgetPassword;
