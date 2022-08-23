00e9b5c0ebfa82674a236545319f0ba5c5dc24b4
Juliasova <juliailopez90@gmail.com>
2022-08-23 16:41:19 +0200

    actualizacion

--- Front/src/index.js
+++ Front/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//import 'bootswatch/dist/minty/bootstrap.min.css'; 
 import './index.css';
 import "bootstrap/dist/js/bootstrap.min.js"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
--- README.md
+++
@@ -1,5 +0,0 @@
-## Sitio de reserva de canchas deportivas!
-
-Sitio web de reservas con acceso a diferentes canchas deportivas (fútbol, tenis, padel).
-
-
