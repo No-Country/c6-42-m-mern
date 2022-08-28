ae22ba1a2a442476651eab99a907ac181e2adf81
requin883 <requin883@gmail.com>
2022-08-28 15:28:44 -0700

    implement functional component for hook usage

--- Front/src/componentes/menu/signIn/Reset2.js
+++ Front/src/componentes/menu/signIn/Reset2.js
@@ -1,106 +1,61 @@
-<<<<<<< Updated upstream
-import React, {useParams} from 'react';
+import React, { useParams, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './modales.css';
 
-function Reset2 (){
+function Reset2() {
+  const [input, setInput] = useState({});
+  const [errors, setErrors] = useState({});
+  const [isValid, setIsValid] = useState(true);
 
-/* constructor() {
-=======
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './modales.css';
-
-class Reset2 extends React.Component{
-
-  constructor() {
->>>>>>> Stashed changes
-    super();
-    this.state = {
-        input: {},
-        errors: {}
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    }
-        
-handleChange(event) {
-    let input = this.state.input;
+  function handleChange(event) {
     input[event.target.name] = event.target.value;
-    this.setState({input});}
-       
-handleSubmit(event) {
-    event.preventDefault();
-      if(this.validate()){
-        console.log(this.state);
-        let input = {};
-        input["password"] = "";
-
-        this.setState({input:input});
-    
-        alert('Su contraseña se restableció correctamente.');
-      }
-    }
-    
-validate(){
-  let input = this.state.input;
-  let errors = {};
-  let isValid = true;
-
-  if (!input["password"]) {
-    isValid = false;
-    errors["password"] = "Por favor, ingrese una nueva contraseña.";
+    this.setState({ input });
   }
 
-this.setState({errors: errors});
-console.log(isValid);
-return isValid;
-<<<<<<< Updated upstream
-} */
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("input:", input);
+    if (validate()) {
+      let input = {};
+      input["password"] = "";
+      setInput({ input });
+      alert('Su contraseña se restableció correctamente.');
+    }
+  }
 
-const [token] = useParams();
-console.log(token);
+  function validate() {
+    if (!input["password"]) {
+      setIsValid(false);
+      errors["password"] = "Por favor, ingrese una nueva contraseña.";
+    }
+    setErrors({ errors });
+    return isValid;
+  }
 
-return(
-<body id="reset">
-<form action='http://localhost:8080/new-password' method='post'>
-=======
+  const [token] = useParams();
+  console.log(token)
+  return (
+    <body id="reset">
+      <form onSubmit={handleSubmit}>
+        <div id="reset2" class="form-group row p-3">
+          <h1>Restablecer contraseña</h1>
+          <div className="col-4" >
+            <input
+              type="password"
+              name="password"
+              value={input.password}
+              onChange={handleChange}
+              className="form-control"
+              placeholder="Ingrese una nueva contraseña" />
+          </div>
+          <div className="text-danger">{errors.password}</div>
+          <div className="text-center p-3">
+            <button type="submit" value="Submit" className="btn btn-success">Enviar</button>
+          </div>
+        </div>
+      </form>
+    </body>
+  )
 }
-
-render(){
-    return(
-<body id="reset">
-<form action='http://localhost:5000/new-password/:resetToken' onSubmit={this.handleSubmit}>
->>>>>>> Stashed changes
-  <div id="reset2" class="form-group row p-3">
-    <h1>Restablecer contraseña</h1>
-    <div className="col-4" >
-    <input
-    type="password"
-    name="password"
-    value={this.state.input.password}
-    onChange={this.handleChange}
-    className="form-control"
-    placeholder="Ingrese una nueva contraseña"/>
-    </div>
-    <div className="text-danger">{this.state.errors.password}</div>    
-<<<<<<< Updated upstream
-=======
-  
-        
->>>>>>> Stashed changes
-    <div className="text-center p-3">
-    <button type="submit" value="Submit"  className="btn btn-success">Enviar</button>
-    </div>   
-  </div>   
-</form>
-</body>
-)
-}
-<<<<<<< Updated upstream
-
-=======
-}
->>>>>>> Stashed changes
 
 export default Reset2;
