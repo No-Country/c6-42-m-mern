a79eb24a441f14c237c1004388ac86c625e73771
requin883 <requin883@gmail.com>
2022-08-28 15:07:09 -0700

    Update login strategy

--- Back/src/Utils/passport-strategies.js
+++ Back/src/Utils/passport-strategies.js
@@ -4,24 +4,21 @@ const userController = require("../controllers/user");
 const userModel = require("../models/userModel");
 
 passport.use('login', new passportStrategy(async (username, password, done) => {
-  console.log(username);
   let user = await userController.exist(username);
-
-  if (!user) return done(null, false);
-  if (!userModel.comparePassword(password, user[0].password)) return done(null, false);
-  
-  return done(null, user[0]);
+  let newMsg = "El usuario o la contraseña son incorrectos";
+  if (!user) return done(null, false,{message:newMsg});
+  const comparedPw = await userModel.comparePassword(password, user[0].password);
+  if (!comparedPw) return done(null, false,{message:newMsg});
+  if (!user.confirmedAccount) return done(null,false,{message:"La cuenta aún no ha sido confirmada"});
+  return done(null,user[0]);
 }));
 
 passport.use('register', new passportStrategy({
   passReqToCallback: true
 }, async (req, username, password, done) => {
-  console.log('Llego strategy')
   let exist = await userController.exist(username);
-
   if (exist) return done("Usuario existente");
   let user = await userController.create(req.body);
-  
   return done(null, user);
 }));
 
