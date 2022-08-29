1204a577e94f34e5bd6e17baf13cbf035ecfa599
requin883 <requin883@gmail.com>
2022-08-28 17:53:47 -0700

    Format text

--- Back/src/routes/routes.js
+++ Back/src/routes/routes.js
@@ -6,6 +6,7 @@ const confirmAccount = require("../middlewares/confirmAccount");
 const router = express.Router();
 const forgetPassword = require("../middlewares/forgetPassword");
 const createNewPass = require("../middlewares/createNewPass");
+const processReservation = require("../middlewares/processReservation");
 
 let isAuth = (req, res, next) => {
     if (req.isAuthenticated()) {
@@ -14,13 +15,14 @@ let isAuth = (req, res, next) => {
     res.redirect('error');
 }
 
-router.put('/new-password/:resetToken',createNewPass);
+router.put('/new-password/:resetToken', createNewPass);
 
-router.get('/activar-cuenta/:verToken',confirmAccount);
+router.get('/activar-cuenta/:verToken', confirmAccount);
 
-router.get('/profile',async (req, res, next) => {
+router.get('/profile', async (req, res, next) => {
     console.log(Object.values(req.sessionStore.sessions)[0].split('"').at(-2));
-    res.send({data:Object.values(req.sessionStore.sessions)[0].split('"').at(-2)})}
+    res.send({ data: Object.values(req.sessionStore.sessions)[0].split('"').at(-2) })
+}
 );
 
 router.get('/error', async (req, res, next) => {
@@ -28,14 +30,14 @@ router.get('/error', async (req, res, next) => {
     res.send(Object.values(req.sessionStore.sessions)[0].split('"').at(-2));
 });
 
-router.put('/forgot-password',forgetPassword);
+router.put('/forgot-password', forgetPassword);
 
 router.get('/jugadores/:id', isAuth, (req, res, next) => {
     console.log(req.params);
     res.send('succeeeessss!');
 });
 
-router.post('/login', passport.authenticate('login', { failureRedirect: 'error', successRedirect: 'profile' , failureMessage:true}));
+router.post('/login', passport.authenticate('login', { failureRedirect: 'error', successRedirect: 'profile', failureMessage: true }));
 
 router.post('/register', passport.authenticate('register', { failureRedirect: 'error' }), async (req, res, next) => {
     try {
@@ -52,25 +54,28 @@ router.get('/logout', (req, res, next) => {
     });
 });
 
-router.get('/activar', async (req, res, next) => {
-    try {
-        // const info = await transporter.sendMail(mailOptions);
-        // console.log(info);
-        res.redirect('http://localhost:3000');
-    } catch (err) {
-        console.log(err);
-        res.send(err);
-    }
+// router.get('/activar', async (req, res, next) => {
+//     try {
+//         // const info = await transporter.sendMail(mailOptions);
+//         // console.log(info);
+//         res.redirect('http://localhost:3000');
+//     } catch (err) {
+//         console.log(err);
+//         res.send(err);
+//     }
 
-});
+// });
+
+router.post('/reservation', processReservation);
 
 router.post('/contact', async (req, res, next) => {
-    try{
-    if (req.body.data){
-    await transporter.sendMail(contactMailOptions(req.body.data));
-    res.status(200).send("Su info ha sido enviada");}
-    res.status(403).send("La información no pudo ser procesada, por favor intente nuevamente");
-    }catch(err){
+    try {
+        if (req.body.data) {
+            await transporter.sendMail(contactMailOptions(req.body.data));
+            res.status(200).send("Su info ha sido enviada");
+        }
+        res.status(403).send("La información no pudo ser procesada, por favor intente nuevamente");
+    } catch (err) {
         console.log(err);
     }
 })
