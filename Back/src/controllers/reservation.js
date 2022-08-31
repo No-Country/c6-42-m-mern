f3436a16c4c94075e572b62bc8387745ca250310
requin883 <requin883@gmail.com>
2022-08-31 05:15:56 -0700

    add stripe functionality and findByDate and delete reservation

--- Back/src/controllers/reservation.js
+++ Back/src/controllers/reservation.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const reservationSchema = require("../models/reservationModel");
 const jwt = require("jsonwebtoken");
 const { transporter, reservationMailOptions } = require("../Utils/nodemailer");
+const stripe = require("stripe")("sk_test_51LcIHQE1aPuiC7C6lKHAWWBh6jmektKy8qPn2AIEKCrxIDsep9mqvGqSKrceSGN3V3CSBILALJMLcze2tEbBcKuY002MPpZHgh");
 class Reservation {
     constructor(collection) {
         this.collection = collection;
@@ -26,6 +27,10 @@ class Reservation {
             quantityOfPlayers,
             totalPrice
         })
+        const session = stripe.checkout.sessions.create({
+            payment_method_types :["card"],
+
+        })
         await transporter.sendMail(reservationMailOptions(newRes));
         const res = await newRes.save();
         return res;
@@ -34,13 +39,22 @@ class Reservation {
     async findByUsername(userN) {
         const model = mongoose.model(this.collection);
         const reservations = await model.find({ username: userN });
-        if (userN.length === 0) return false;
+        if (reservations.length === 0) return false;
         return reservations;
     };
 
-    findAll() { };
+    async findByDate(ReceivedDate) {
+        const model = mongoose.model(this.collection);
+        const reservations = await model.find({date:ReceivedDate});
+        if (reservations.length === 0) return false;
+        return reservations;
+    };
 
-    delete() { };
+    async delete(id) {
+        const model = mongoose.model(this.collection);
+        const deletedReservation = await model.findOneAndDelete({_id:id})
+        return deletedReservation;
+     };
 
     update() { };
 };
