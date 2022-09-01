a4aa41eb2d4acdeb3c8165cca19f01abe3511431
requin883 <requin883@gmail.com>
2022-08-31 21:06:31 -0700

    Create reservationMailOptions

--- Back/src/Utils/nodemailer.js
+++ Back/src/Utils/nodemailer.js
@@ -9,12 +9,24 @@ const transporter = createTransport({
   }
 });
 
-const contactMailOptions = ({ name, email, subject, message }) => {
+const contactMailOptions = ({ name, email, club, subject, message }) => {
   return {
     from: `${name} -- ${email}`,
-    to: email,
+    to: process.env.NODEMAILER_USER,
     subject,
-    text: message
+    html: `<h3>${name} envia el siguiente mensaje para el club: ${club}<h3><br> <h3>${message}<h3>`
+  }
+}
+
+const reservationMailOptions = ({username, email,date,time,sport,courtName,quantityOfPlayers, totalPrice})=>{
+  return{
+    from:username,
+    to:email,
+    bbc:process.env.NODEMAILER_USER,
+    subject:`Confirmación de reserva de ${username} el día ${date} a las ${time}`,
+    text:`${username}! Este correo es para confirmar tu reserva el día ${date} a las ${time} en la cancha ${courtName}.
+    En total serían ${quantityOfPlayers} jugadores para jugar ${sport}.
+    `
   }
 }
 
@@ -40,4 +52,4 @@ const mailOptions = (confirmationLink, userEmail, option) => {
   }
 }
 
-module.exports = { transporter, mailOptions, contactMailOptions };
+module.exports = { transporter, mailOptions, contactMailOptions, reservationMailOptions };
