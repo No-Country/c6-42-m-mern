e9ff2f05db84056af86553212003de767d084b5c
requin883 <requin883@gmail.com>
2022-09-01 19:07:34 -0700

    format

--- Back/src/models/courtMsgModel.js
+++ Back/src/models/courtMsgModel.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const courtMsgSchema = new mongoose.Schema({
     name: String,
-    email:String,
-    club:String,
-    subject:String,
-    message:String,
+    email: String,
+    club: String,
+    subject: String,
+    message: String,
     date: {
         type: Date,
         default: Date.now()
