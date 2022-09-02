74d8361945d450ee92a45b4293bda764df877eb3
requin883 <requin883@gmail.com>
2022-09-01 19:12:51 -0700

    remove email, change courtName to court and change totalPrice from Number to String

--- Back/src/models/reservationModel.js
+++ Back/src/models/reservationModel.js
@@ -3,16 +3,18 @@ const mongoose = require("mongoose");
 const reservationSchema = new mongoose.Schema(
     {
         username: String,
-        email: String,
         date: String,
         time: String,
         paymentMethod: String,
         sport: String,
-        courtName: String,
-        quantityOfPlayers: Number,
-        totalPrice: Number,
+        court: String,
+        totalPrice: String
     },
-    { timestamps: true }
+    {
+        timestamps: true,
+        collection: 'reservas'
+    }
+
 );
 
 module.exports = new mongoose.model('reservas', reservationSchema);
