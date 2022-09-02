b4dac5b03d6fa2b7485f1d38dfc41bcc364699d7
requin883 <requin883@gmail.com>
2022-09-01 19:18:31 -0700

    Add again "reservas" section

--- Front/src/componentes/login/jugador/home/Home2.js
+++ Front/src/componentes/login/jugador/home/Home2.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Menu from '../menu/Menu';
 import Perfil from './miperfil/Miperfil.js'
 import Footer from '../footer/Footer';
-// import Reservas from './reservas/Reservas';
+import Reservas from './reservas/Reservas';
 import Contacto from './contacto/Contacto';
 
 
@@ -10,7 +10,7 @@ const Home2 = () => {
 	return <>
 		<section id="Menu"><Menu /></section>
 		<section id="Perfil"><Perfil /></section>
-		{/* <section id="Reservas"><Reservas /></section> */}
+		<section id="Reservas"><Reservas /></section>
 		<section id="Contacto"><Contacto /></section>
 		<section id="Footer"><Footer /></section>
 	</>
